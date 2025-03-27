94f5dd6499d13df37fae2a8f1a2b49833a8a9f7e
gtugaw <tugawinga@gmail.com>
2025-03-27 14:35:43 +0000

    another update

--- comp1236.sh
+++ comp1236.sh
@@ -1,13 +1,14 @@
 # This script calculates the sum or
 # the difference of the two numbers
+
 read -p "Enter the first number: " num1
 read -p "Enter the second number: " num2
 
 if [ $num1 -gt $num2 ]
 then
-    result=$((num1 + num2))
+    sum=$((num1 + num2))
+    echo "The sum of $num1 and $num2 is $sum. "
 else
-    result=$((num2 - num1))
+    diff=$((num2 - num1))
+    echo "The difference of $num1 and $num2 is $diff. "
 fi
-
-echo "The result is $result. "
