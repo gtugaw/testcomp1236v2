782b5b6fefc31333a35588e98dd91d9a50c68f94
gtugaw <tugawinga@gmail.com>
2025-03-27 15:57:13 +0000

    add exer pseudocode

---
+++ exer12_09.sh
@@ -0,0 +1,15 @@
+# Case logic is often used when many choices are given through a program or when
+# many responses can be made on the basis of one choice. In this project, you create a
+# shell script that employs case logic to respond to your favorite color (many possible
+#responses selected on the basis of one choice). The choices for the user are: blue,
+# yellow, red, orange. Print a message of your choice in response to each coloured
+# selected by the user
+
+read color
+
+case $color in
+red) echo "Red apple. "
+blue)
+yellow)
+*)
+esac)
