c98f24cbe8f263c7e2fef85b5a83b26c1a6015c2
gtugaw <tugawinga@gmail.com>
2025-03-27 14:22:13 +0000

    just an update

--- comp1236.sh
+++ comp1236.sh
@@ -1,2 +1,11 @@
-# This is just a comment
-echo "Hello World!"
+# This script calculates the sum or
+# the difference of the two numbers
+read -p "Enter the first number: " num1
+read -p "Enter the second number: " num2
+
+if [ $num1 -gt $num2 ]
+then
+echo "First number is greater than the second number."
+else
+echo "First number is not greater than the second number."
+fi
